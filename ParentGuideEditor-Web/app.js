7ea25b00c41a4f0b8779db5308ecaf7c7c4d129d
ali198724 <ali198724@gmail.com>
2020-08-28 22:54:54 +0300

    hide controls on startup

--- ParentGuideEditor-Web/app.js
+++ ParentGuideEditor-Web/app.js
@@ -12,6 +12,9 @@ const GUIDE_STATE = {
 var parentGuideClass = null;
 var video_state = VIDEO_STATE.STOP;
 var guide_state = GUIDE_STATE.ON;
+var edit_table = false;
+var edit_file = false;
+var edit_control = false;
 
 document.addEventListener('DOMContentLoaded', function(event){
     init();
@@ -20,6 +23,12 @@ document.addEventListener('DOMContentLoaded', function(event){
     var stop = document.getElementById("btn_stop");
     var guide = document.getElementById("btn_guide");
     var lbl = document.getElementById("lbl_time");
+    var edit = document.getElementById("btn_edit");
+    var btn_document = document.getElementById("btn_document");
+    var control = document.getElementById("btn_control");
+    var div_tb = document.getElementById("div_tb");
+    var div_document = document.getElementById("div_document");
+    var div_control = document.getElementById("div_control");
     setInterval(function(){
         var violence = (document.getElementById("sel_Violence").value == "Blur") ? '-webkit-filter: blur(5px);' : '-webkit-filter: opacity(0%);';
         var nudity = (document.getElementById("sel_Nudity").value == "Blur") ? '-webkit-filter: blur(5px);' : '-webkit-filter: opacity(0%);';
@@ -84,6 +93,39 @@ document.addEventListener('DOMContentLoaded', function(event){
             guide_state = GUIDE_STATE.ON;
             guide.style.opacity = "100%";
         }
+    });
+    edit.addEventListener("click", function(){
+        if(edit_table === true){
+            edit.style.opacity = "50%";
+            div_tb.style.display = "none";
+        }
+        else{
+            edit.style.opacity = "100%";
+            div_tb.style.display = "inherit";
+        }
+        edit_table = !edit_table;
+    });
+    btn_document.addEventListener("click", function(){
+        if(edit_file === true){
+            btn_document.style.opacity = "50%";
+            div_document.style.display = "none";
+        }
+        else{
+            btn_document.style.opacity = "100%";
+            div_document.style.display = "inherit";
+        }
+        edit_file = !edit_file;
+    });
+    control.addEventListener("click", function(){
+        if(edit_control === true){
+            control.style.opacity = "50%";
+            div_control.style.display = "none";
+        }
+        else{
+            control.style.opacity = "100%";
+            div_control.style.display = "inherit";
+        }
+        edit_control = !edit_control;
     });
 });
 
